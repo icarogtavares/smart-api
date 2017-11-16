7c64f14729b09a5c9a5c044dc0897b48ae8535f8
Icaro Tavares <icarotavares@live.com>
2017-11-16 15:09:37 -0300

    Updated noise model

--- server/src/models/noise.js
+++ server/src/models/noise.js
@@ -1,12 +1,5 @@
 export default (sequelize, DataTypes) => {
   let noise = sequelize.define('noise', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
     sound_level: {
       type: DataTypes.DECIMAL(10,6),
       allowNull: false,
