a34bf03d9ab531c2ecb79c5bad07cc76ed69b248
Icaro Tavares <icarotavares@live.com>
2017-11-17 14:42:59 -0300

    Checking noise received
    checking object noise received by the publisher.
    next step: check if is an error message

--- server/src/mqtt/index.js
+++ server/src/mqtt/index.js
@@ -1,4 +1,5 @@
 import mqtt from 'mqtt'
+import { has } from 'ramda'
 import * as noisesService from '../services/noises'
 
 const client = mqtt.connect('mqtt://test.mosquitto.org')
@@ -8,21 +9,26 @@ export const start_connection = () => {
     client.on('connect', () => {
         client.subscribe(TOPIC_NOISE)
         client.publish(TOPIC_NOISE, "{\"sound_level\": 16}")
+        client.publish(TOPIC_NOISE, "{\"error\": -2}")
     })
 
     client.on('message', (topic, message) => {
         switch (topic) {
         case TOPIC_NOISE:
             return handleTopicNoise(message)
-        //   case 'xxxx/yyyy':
-        //     return handleXXXXYYYYState(message)
         }
         console.log('No handler for topic %s', topic)
     })
 
     function handleTopicNoise (message) {
         const noise = JSON.parse(message.toString())
-        noisesService.create(noise);
-        console.log(noise)
+        const hasSoundLevel = has('sound_level');
+        const hasError = has('error');
+        if(hasSoundLevel(noise)) {
+            noisesService.create(noise);
+        } else if(hasError(noise)) {
+
+        }
+    
     }
 }
