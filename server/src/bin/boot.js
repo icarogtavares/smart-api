c1f5f152eba3cd55a43774915e6a4d42f80e57ed
Icaro Tavares <icarotavares@live.com>
2017-12-10 16:51:56 -0300

    Ajustes

--- server/src/bin/boot.js
+++ server/src/bin/boot.js
@@ -10,5 +10,5 @@ export const startApp = (id) => {
     smartDebug(`Listening on port ${app.get('port')}`);
   });
 
-  mqtt.start_connection();
+  mqtt.startConnection();
 }
--- server/src/mqtt/index.js
+++ server/src/mqtt/index.js
@@ -5,9 +5,10 @@ import * as errorsService from '../services/errors'
 const client = mqtt.connect('mqtt://' + process.env.MQTT || 'test.mosquitto.org')
 const TOPIC_NOISE = process.env.T || 'SMART/NOISE'
 
-export const start_connection = () => {
+export const startConnection = () => {
     client.on('connect', () => {
         client.subscribe(TOPIC_NOISE)
+        console.log("MQTT CONNECTED");
     })
 
     client.on('message', (topic, message) => {
@@ -19,14 +20,20 @@ export const start_connection = () => {
     })
 
     function handleTopicNoise (message) {
-        const messageJson = JSON.parse(message.toString());
-        const messageHas = has(__, messageJson);
-        if(messageHas('sound_level')) {
-            noisesService.create(messageJson).catch(err => console.log(err));
-        } else if(messageHas('error')) {
-            messageJson.code = messageJson.error;
-            delete messageJson.error;
-            errorsService.increment(messageJson).catch(err => console.log(err));
+        try {
+            const messageJson = JSON.parse(message.toString());
+            const messageHas = has(__, messageJson);
+            if(messageHas('sound_level')) {
+                noisesService.create(messageJson).catch(err => console.log(err));
+            } else if(messageHas('error')) {
+                messageJson.code = messageJson.error;
+                delete messageJson.error;
+                errorsService.increment(messageJson).catch(err => console.log(err));
+            } else {
+                console.log("RECEIVED " + message.toString());
+            }
+        } catch(err) {
+            console.log("MQTT Received invalid json message.");
         }
     
     }
